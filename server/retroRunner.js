236b4d6d2daba2a6fbbad231b783d8a56494caa9
Greg Walker <mgwalker@gmail.com>
2016-07-28 13:03:18 -0500

    Pull out the retro steps into functions

--- server/retroRunner.js
+++ server/retroRunner.js
@@ -10,29 +10,60 @@ class RetroRunner {
     this.socket.emit('starting retro', 10);
     this.categoryIndex = 0;
     this.voting = false;
-    setTimeout(this.next.bind(this), 10000);
+    setTimeout(() => { this.startPolling(this.getDelays()); }, 10000);
   }
 
-  next() {
+  getCurrentCategory() {
+    return this.retro.categories[this.categoryIndex];
+  }
+
+  getDelays() {
+    const vote = this.retro.categoryTimes[this.getCurrentCategory()].voteTime * 60000;
+    const poll = this.retro.categoryTimes[this.getCurrentCategory()].selfTime * 60000;
+    return {
+      vote, poll
+    };
+  }
+
+  startPolling(delays) {
+    const category = this.getCurrentCategory();
+    this.socket.emit('polling', category);
+    console.log(`Polling: ${category}`);
+    setTimeout(() => this.tenSecondWarning(), delays.poll - 10000);
+    setTimeout(() => this.collectAnswers(delays), delays.poll);
+  }
+
+  collectAnswers(delays) {
+    const category = this.getCurrentCategory();
+    this.socket.emit('collect answers', category);
+    console.log(`Collecting answers: ${category}`);
+    setTimeout(() => this.startVoting(delays), 3000);
+  }
+
+  startVoting(delays) {
+    const category = this.getCurrentCategory();
+    this.socket.emit('voting', { category, entries: [] });
+    console.log(`Voting: ${category}`);
+    setTimeout(() => this.tenSecondWarning(), delays.vote - 10000);
+    setTimeout(() => this.collectVotes(), delays.vote);
+  }
+
+  collectVotes() {
+    const category = this.getCurrentCategory();
+    this.socket.emit('collect votes', category);
+    console.log(`Collecting votes: ${category}`);
+    setTimeout(() => this.nextCategory(), 3000);
+  }
+
+  tenSecondWarning() {
+    this.socket.emit('10 second warning');
+    console.log('10-second warning');
+  }
+
+  nextCategory() {
+    this.categoryIndex++;
     if (this.categoryIndex < this.retro.categories.length) {
-      const category = this.retro.categories[this.categoryIndex];
-      let delay = 0;
-
-      if (this.voting) {
-        this.socket.emit('voting', { category, entries: [] });
-        console.log(`Voting: ${category}`);
-        delay = this.retro.categoryTimes[category].voteTime;
-        this.categoryIndex++;
-      } else {
-        this.socket.emit('polling', category);
-        console.log(`Polling: ${category}`);
-        delay = this.retro.categoryTimes[category].selfTime;
-      }
-
-      this.voting = !this.voting;
-      delay = delay * 60000;
-      setTimeout(() => { this.socket.emit('10 second warning'); }, delay - 10000);
-      setTimeout(this.next.bind(this), delay);
+      this.startPolling(this.getDelays());
     } else {
       console.log('All done!');
     }
