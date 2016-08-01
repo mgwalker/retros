fdfd75cd6b96e2d91d04e4f72107679a1f752bda
Greg Walker <mgwalker@gmail.com>
2016-07-31 20:42:46 -0700

    Move docker stuff into a directory for deployment's sake

--- docker/docker-start.sh
+++ docker/docker-start.sh
@@ -6,5 +6,5 @@ if [ $? -eq 0 ]
 then
   docker restart retros-run
 else
-  docker run --name retros-run -p 23526:23526 -d -v `pwd`:/usr/share/retros retros
+  docker run --name retros-run -p 23526:23526 -d -v `pwd`/..:/usr/share/retros retros
 fi
